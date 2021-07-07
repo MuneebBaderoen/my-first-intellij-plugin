8dfb1f7998bf56f35a726c83256c3b3536ad3d9c
GitHub Action <action@github.com>
2021-07-07 00:33:34 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/muneebbaderoen/myfirstintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.muneebbaderoen.myfirstintellijplugin.services
+
+import com.github.muneebbaderoen.myfirstintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/muneebbaderoen/myfirstintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/muneebbaderoen/myfirstintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.muneebbaderoen.myfirstintellijplugin.services
 
+import com.github.muneebbaderoen.myfirstintellijplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
